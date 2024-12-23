1210a820aeec83296378ec1ee1936512bb68c006
imaneboho2159 <imaboh21@gmail.com>
2024-12-23 10:35:44 +0100

    make some changes

--- src/Application.java
+++ src/Application.java
@@ -20,18 +20,18 @@ public  class Application {
     }
 
     public  void ManagementAap() {
-
+        int choice =1;
+        while (choice!=0) {
         System.out.println("\n=== Menu Principal ===");
         System.out.println("1. Management Student");
         System.out.println("2. Management Trainer");
         System.out.println("3. Management Class");
         System.out.println("0. Get out");
         System.out.print("Enter choice: ");
-        int choice = scanner.nextInt();
+         choice= scanner.nextInt();
         scanner.nextLine();
 
-        while (choice!=0) {
-            switch (choice) {
+        switch (choice) {
                 case 1:
                     MenuStudent();
                     break;
